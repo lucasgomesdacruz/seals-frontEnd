69ebe124ccb04a48625b945728b5c23acf407239
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-04 11:46:02 -0300

    fix: adiciona seo

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.module.scss'
+import './index.scss'
 
 import { router } from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
